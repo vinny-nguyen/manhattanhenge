a570d92159810a153bf9538a953c468dcc28b592
Vincent Nguyen <phucvn166@gmail.com>
2025-11-22 01:50:51 -0500

    setting up mapboxgl

---
+++ manhattanhenge/app/components/map.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+// Imports:
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = "MAPBOXGL";
+
+export default function Map() {
+    const mapContainer = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!mapContainer.current) return;
+        const map = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/light-v11",
+            center: [-73.9857, 40.7484],
+            zoom: 12,
+        });
+
+        return () => map.remove();
+    }, []);
+
+    return (
+        <div ref={mapContainer} 
+        className="w-full h-96 rounded border border-black/[.08]"
+        />
+    );
+}
